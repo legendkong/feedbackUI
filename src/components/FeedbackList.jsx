2d7767d3d94f8e8397a96ab35b9dcb487526a996
Jonathan Kong <jonathankong96@hotmail.com>
2022-04-23 03:05:22 +0800

    Created mock JSON server, implemented fetch request

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,17 +2,16 @@ import {motion, AnimatePresence} from 'framer-motion'
 import { useContext } from 'react'
 import FeedbackItem from "./FeedbackItem"
 import FeedbackContext from '../context/FeedbackContext'
+import Spinner from './shared/Spinner'
 
 function FeedbackList() {
 
-   const {feedback} = useContext(FeedbackContext)
+   const {feedback, isLoading } = useContext(FeedbackContext)
 
-   if(!feedback || feedback.length === 0) {
+   if(!isLoading && (!feedback || feedback.length === 0)) {
       return <p>No Feedback Yet</p>
    }
-
-      return (
-      <div className="feedback-list">
+      return isLoading ? <Spinner /> : (<div className="feedback-list">
          <AnimatePresence>
          {feedback.map((item) => (
             <motion.div 
@@ -27,18 +26,7 @@ function FeedbackList() {
             </motion.div>
          ))}
          </AnimatePresence>
-      </div>
-
-   // return (
-   //    <div className="feedback-list">
-   //       {feedback.map((item) => (
-   //          <FeedbackItem 
-   //          key={item.id} 
-   //          item={item}
-   //          handleDelete={handleDelete}/>
-   //       ))}
-   //    </div>
-   )
+      </div>)
 }
 
 export default FeedbackList
