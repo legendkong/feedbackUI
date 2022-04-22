7966d0b6684a00dc446b5399eb35111c921ab82e
Jonathan Kong <jonathankong96@hotmail.com>
2022-04-23 00:36:02 +0800

    Context api

--- src/App.js
+++ src/App.js
@@ -1,40 +1,27 @@
-import { v4 as uuidv4 } from 'uuid'
+
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
-import { useState } from "react"
-import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
 import AboutIconLink from './components/AboutIconLink'
+import {FeedbackProvider} from './context/FeedbackContext'
 import AboutPage from './pages/AboutPage'
 
 
 function App() {
-   const [feedback, setFeedback] = useState(FeedbackData)
-
-   const addFeedback = (newFeedback) => {
-      newFeedback.id = uuidv4()
-      setFeedback([newFeedback, ...feedback])
-   }
-
-   const deleteFeedback = (id) => {
-      if(window.confirm('Are you sure you want to delete?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
-      }
-   }
-
+   
    return (
+      <FeedbackProvider>
       <Router>
          <Header />
          <div className="container">
             <Routes>
                <Route exact path='/' element={
                   <>
-                  <FeedbackForm handleAdd={addFeedback}/>
-                  <FeedbackStats feedback={feedback} />
-                  <FeedbackList feedback={feedback}
-                  handleDelete={deleteFeedback}/>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
                   </>
                }>
 
@@ -45,6 +32,7 @@ function App() {
             <AboutIconLink />
          </div>
       </Router>
+   </FeedbackProvider>
    )
 }
 
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,15 +1,26 @@
-import { useState } from "react"
+import { useState, useContext, useEffect } from "react"
 import Card from "./shared/Card"
 import Button from "./shared/Button"
 import RatingSelect from "./RatingSelect"
+import FeedbackContext from "../context/FeedbackContext"
 
 
-function FeedbackForm({handleAdd}) {
+function FeedbackForm() {
 
    const [text, setText] = useState('')
    const [rating, setRating] = useState(10)
    const [btnDisabled, setBtnDisabled] = useState(true)
    const [message, setMessage] = useState('')
+
+   const {addFeedback, feedbackEdit } = useContext(FeedbackContext)
+
+   useEffect(() => {
+      if(feedbackEdit.edit === true){
+         setBtnDisabled(false)
+         setText(feedbackEdit.item.text)
+         setRating(feedbackEdit.item.rating)
+      }
+   }, [feedbackEdit])
 
    const handleTextChange = (e) => {
 if(text === '') {
@@ -34,7 +45,7 @@ const handleSubmit = (e) => {
          rating
       }
 
-      handleAdd(newFeedback)
+      addFeedback(newFeedback)
 
       setText('')
    }
